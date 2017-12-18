07f4ff104ece7326217cd1afa217626622cddcdc
Zhe Li <zlhe@microsoft.com>
2017-12-18 16:29:39 +0800

    override exception helper

--- src/exceptionHelper.ts
+++ src/exceptionHelper.ts
@@ -2,10 +2,24 @@
 
 import * as vscode from 'vscode';
 
-export function exceptionHelper(error: Error, popupMsgbox: boolean) {
-  if (popupMsgbox) {
-    vscode.window.showErrorMessage(error.message);
+export function exceptionHelper(error: string, popupMsgbox: boolean): void;
+export function exceptionHelper(error: Error, popupMsgbox: boolean): void;
+export function exceptionHelper(error: string | Error, popupMsgbox: boolean): void {
+  let _error: Error;
+  let _message: string;
+
+  if (typeof error === 'string') {
+    _error = new Error(error);
+    _message = error;
+  } else {
+    _error = error;
+    _message = error.message;
   }
 
-  console.error(error);
+  if (popupMsgbox) {
+    vscode.window.showErrorMessage(_message);
+  }
+
+  console.error(_error);
+  process.exit(1);
 }
