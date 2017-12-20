ce39fc37c3664ba58ad223d82f6a17d90b761d40
Zhe Li <zlhe@microsoft.com>
2017-12-20 11:31:26 +0800

    save

--- src/Models/Apis.ts
+++ src/Models/Apis.ts
@@ -4,8 +4,8 @@ import {ApiName} from './Interfaces/Api';
 export function getExtensionApi(name: ApiName): {}|null {
   switch (name) {
     case ApiName.Toolkit:
-      return vscode.extensions.getExtension(
-                                  'vsciot-vscode.azure-iot-toolkit')!.exports;
+      return vscode.extensions.getExtension('vsciot-vscode.azure-iot-toolkit')!
+          .exports.azureIoTExplorer;
     default:
       return null;
   }
--- src/Models/IoTHub.ts
+++ src/Models/IoTHub.ts
@@ -1,3 +1,7 @@
+import * as vscode from 'vscode';
+
+import {exceptionHelper} from '../exceptionHelper';
+
 import {getExtensionApi} from './Apis';
 import {ApiName} from './Interfaces/Api';
 import {Component, ComponentType} from './Interfaces/Component';
@@ -23,12 +27,49 @@ export class IoTHub implements Component, Provisionable {
   }
 
   async provision(): Promise<boolean> {
-    const toolkitApi = getExtensionApi(ApiName.Toolkit);
-    const iothub = await toolkitApi.azureIoTExplorer.createIoTHub();
-
     return new Promise(
-        (resolve: (value: boolean) => void,
-         reject: (value: boolean) => void) => {
+        async (
+            resolve: (value: boolean) => void,
+            reject: (value: boolean) => void) => {
+          const provisionIothubSelection: vscode.QuickPickItem[] = [
+            {
+              label: 'Select an existed IoT Hub',
+              description: 'Select an existed IoT Hub',
+              detail: 'select'
+            },
+            {
+              label: 'Create a new IoT Hub',
+              description: 'Create a new IoT Hub',
+              detail: 'create'
+            }
+          ];
+          const selection = await vscode.window.showQuickPick(
+              provisionIothubSelection,
+              {ignoreFocusOut: true, placeHolder: 'Provision IoT Hub'});
+
+          if (!selection) {
+            reject(false);
+            return;
+          }
+
+          const toolkitApi = getExtensionApi(ApiName.Toolkit);
+          if (toolkitApi === null) {
+            reject(false);
+            return;
+          }
+
+          let iothub = null;
+          switch (selection.detail) {
+            case 'select':
+              iothub = await toolkitApi.selectIoTHub();
+              break;
+            case 'create':
+              iothub = await toolkitApi.createIoTHub();
+              break;
+            default:
+              break;
+          }
+
           if (iothub && iothub.name) {
             // TODO: iothub & handle device
             resolve(true);
