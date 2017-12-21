24aeb09d0822f127c5de6ae304fe8ba9902b8312
FAREAST\dol <dol@microsoft.com>
2017-12-21 17:12:17 +0800

    implement create device

--- src/Models/AZ3166Device.ts
+++ src/Models/AZ3166Device.ts
@@ -1,9 +1,16 @@
+import * as fs from 'fs-plus';
+import * as path from 'path';
 import * as vscode from 'vscode';
-
-import {ExceptionHelper} from '../exceptionHelper';
-
 import {Component, ComponentType} from './Interfaces/Component';
 import {Device, DeviceType} from './Interfaces/Device';
+import {ExceptionHelper} from '../exceptionHelper';
+
+const constants = {
+  defaltSketchFileName: 'myDevice.ino',
+  arduinoJsonFileName: 'arduino.json',
+  boardInfo: 'AZ3166:stm32f4:MXCHIP_AZ3166',
+  uploadMethod: 'upload_method=OpenOCDMethod'
+};
 
 export class AZ3166Device implements Device {
   private deviceType: DeviceType;
@@ -29,6 +36,43 @@ export class AZ3166Device implements Device {
   }
 
   create(): boolean {
+    const rootPath: string = vscode.workspace.rootPath as string;
+    const deviceFolderPath = path.join(rootPath, this.deviceFolder);
+
+    if (!fs.existsSync(deviceFolderPath)) {
+      ExceptionHelper.logError(`Device folder doesn't exist: ${deviceFolderPath}`, true);
+    }
+
+    // Get arduino sketch file name from user input or use defalt sketch name
+    const option: vscode.InputBoxOptions = {
+      value: constants.defaltSketchFileName,
+      prompt: `Please input device sketch file name here.`,
+      ignoreFocusOut: true
+    };
+
+    vscode.window
+      .showInputBox(option)
+      .then(val => {
+        const sketchFileName = val;
+
+        // Create arduino.json config file
+        const arduinoJsonFilePath = path.join(deviceFolderPath, constants.arduinoJsonFileName);
+        const arduinoJsonObj = {
+          'board': constants.boardInfo,
+          'stetch': sketchFileName,
+          'configuration': constants.uploadMethod
+        };
+
+        try
+        {
+          fs.writeFileSync(arduinoJsonFilePath, JSON.stringify(arduinoJsonObj, null, 4));
+        }
+        catch (error)
+        {
+          ExceptionHelper.logError(`Device: create arduino config file failed: ${error.message}`, true);
+        }
+    });
+
     return true;
   }
 
