d64743c26a01bb93ce18b951f680e10407629ae0
Zhe Li <Sneezry@users.noreply.github.com>
2017-12-21 13:15:37 +0800

    match gts rule (#19)

--- src/exceptionHelper.ts
+++ src/exceptionHelper.ts
@@ -5,11 +5,10 @@ import * as vscode from 'vscode';
 export class ExceptionHelper {
   static logError(error: string, popupMsgbox: boolean): void;
   static logError(error: Error, popupMsgbox: boolean): void;
-  static logError(
-      error: string|Error, popupMsgbox: boolean): void {
+  static logError(error: string|Error, popupMsgbox: boolean): void {
     let _error: Error;
     let _message: string;
-  
+
     if (typeof error === 'string') {
       _error = new Error(error);
       _message = error;
@@ -17,11 +16,11 @@ export class ExceptionHelper {
       _error = error;
       _message = error.message;
     }
-  
+
     if (popupMsgbox) {
       vscode.window.showErrorMessage(_message);
     }
-  
+
     throw _error;
   }
 }
