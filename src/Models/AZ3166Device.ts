cc85a5f1aaa17c2152afa2e27319de4b378b5094
FAREAST\dol <dol@microsoft.com>
2018-01-09 12:07:03 +0800

    fix configuration mode prompt issue

--- src/Models/AZ3166Device.ts
+++ src/Models/AZ3166Device.ts
@@ -379,18 +379,18 @@ export class AZ3166Device implements Device {
 
           setTimeout(() => {
             if (errorRejected) return;
-            if (!gotData || !configMode) {
-              // Prompt user to enter configuration mode
-              vscode.window
-                  .showInformationMessage(
-                      'Please hold down button A and then push and release the reset button to enter configuration mode.')
-                  .then(() => {
+            // Prompt user to enter configuration mode
+            vscode.window
+                .showInformationMessage(
+                    'Please hold down button A and then push and release the reset button to enter configuration mode.')
+                .then(() => {
+                  if (!gotData || !configMode) {
                     // tslint:disable-next-line: no-any
                     port.write('\r\nhelp\r\n', (error: any) => {
                       rejectIfError(error);
                     });
-                  });
-            }
+                  }
+                });
           }, 10000);
         });
   }
