e1218f95b2e8ecec8d40de46e533c33beffb143f
Renhe Li <renhe.li@hotmail.com>
2018-02-22 10:19:38 +0800

    Add cpp properties file when initialize project  (#60)
    * Add cpp properties file when initialize project
    
    * Generate root path for win32
    
    * Add more template and code snippet for extension

--- src/Models/AZ3166Device.ts
+++ src/Models/AZ3166Device.ts
@@ -33,7 +33,10 @@ const constants = {
   arduinoJsonFileName: 'arduino.json',
   boardInfo: 'AZ3166:stm32f4:MXCHIP_AZ3166',
   uploadMethod: 'upload_method=OpenOCDMethod',
-  resourcesFolderName: 'resources'
+  resourcesFolderName: 'resources',
+  cppPropertiesFileName: 'c_cpp_properties.json',
+  cppPropertiesFileNameMac: 'c_cpp_properties_macos.json',
+  cppPropertiesFileNameWin: 'c_cpp_properties_win32.json',
 };
 
 export class AZ3166Device implements Device {
@@ -113,7 +116,6 @@ export class AZ3166Device implements Device {
         if (fileExt !== 'ino') {
           val = val + '.ino';
         }
-
         sketchFileName = val;
       }
 
@@ -134,6 +136,41 @@ export class AZ3166Device implements Device {
             `Device: create arduino config file failed: ${error.message}`);
       }
 
+      // Create c_cpp_properties.json file
+      const cppPropertiesFilePath =
+          path.join(vscodeFolderPath, constants.cppPropertiesFileName);
+
+      try {
+        const plat = os.platform();
+
+        if (plat === 'win32') {
+          const propertiesFilePathWin32 =
+              this.extensionContext.asAbsolutePath(path.join(
+                  constants.resourcesFolderName,
+                  constants.cppPropertiesFileNameWin));
+          const propertiesContentWin32 =
+              fs.readFileSync(propertiesFilePathWin32).toString();
+          const pattern = /{ROOTPATH}/gi;
+          const localAppData: string = process.env.LOCALAPPDATA;
+          const replaceStr = propertiesContentWin32.replace(
+              pattern, localAppData.replace(/\\/g, '\\\\'));
+          fs.writeFileSync(cppPropertiesFilePath, replaceStr);
+        }
+        // TODO: Let's use the same file for Linux and MacOS for now. Need to
+        // revisit this part.
+        else {
+          const propertiesFilePathMac =
+              this.extensionContext.asAbsolutePath(path.join(
+                  constants.resourcesFolderName,
+                  constants.cppPropertiesFileNameMac));
+          const propertiesContentMac =
+              fs.readFileSync(propertiesFilePathMac).toString();
+          fs.writeFileSync(cppPropertiesFilePath, propertiesContentMac);
+        }
+      } catch (error) {
+        throw new Error(`Create cpp properties file failed: ${error.message}`);
+      }
+
       // Create an empty arduino sketch
       const sketchTemplateFilePath = this.extensionContext.asAbsolutePath(
           path.join(constants.resourcesFolderName, this.sketchName));
@@ -147,7 +184,7 @@ export class AZ3166Device implements Device {
           vscode.window.showTextDocument(newFileUri);
         }
       } catch (error) {
-        throw new Error('Create arduino sketch file failed.');
+        throw new Error(`Create arduino sketch file failed: ${error.message}`);
       }
     });
 
