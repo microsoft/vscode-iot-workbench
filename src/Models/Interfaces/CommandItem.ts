2086c4facf99ba6ce64550549ac4f7d1d99cb69c
Zhe Li <zlhe@microsoft.com>
2018-03-09 16:20:20 +0800

    define menu

--- src/Models/Interfaces/CommandItem.ts
+++ src/Models/Interfaces/CommandItem.ts
@@ -1,7 +1,19 @@
 import * as vscode from 'vscode';
 
 export interface CommandItem extends vscode.QuickPickItem {
+  /**
+   * Click action of the menu item
+   */
   // tslint:disable-next-line: no-any
   onClick?: (...args: any[]) => any;
+  /**
+   * Submenu of the menu item
+   */
   children?: CommandItem[];
+  /**
+   * Show the meu item when only the
+   * workspace configuration contains
+   * the specific field.
+   */
+  only?: string;
 }
