b292abab99c9bc5a7c54b640b9b3c21e2e77e406
system_user <lizhe@lizhe.org>
2018-04-08 10:44:58 +0800

    fix no initializer issue in test

--- test/stub.ts
+++ test/stub.ts
@@ -1,12 +1,23 @@
 import * as vscode from 'vscode';
 
+class DummyMemento implements vscode.Memento {
+  get<T>(key: string): Promise<T|undefined> {
+    return Promise.resolve(undefined);
+  }
+
+  // tslint:disable-next-line: no-any
+  update(key: string, value: any): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
 // mock of the extension context for vscode
 class TestExtensionContext implements vscode.ExtensionContext {
-  subscriptions: Array<{dispose(): {}}>;
-  workspaceState: vscode.Memento;
-  globalState: vscode.Memento;
-  extensionPath: string;
-  storagePath: string;
+  subscriptions: Array<{dispose(): {}}> = [];
+  workspaceState: vscode.Memento = new DummyMemento();
+  globalState: vscode.Memento = new DummyMemento();
+  extensionPath = '';
+  storagePath = '';
 
   asAbsolutePath(relativePath: string): string {
     return '';
