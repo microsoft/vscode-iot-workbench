fa79d2ea8dd2f9b4a4da31e8e2a8101a657a7f99
Renhe Li <renhel@microsoft.com>
2018-04-24 10:48:43 +0800

    Provide the API of config device settings and move it to interface

--- src/DeviceOperator.ts
+++ src/DeviceOperator.ts
@@ -52,7 +52,7 @@ export class DeviceOperator {
     await project.upload();
   }
 
-  async setConnectionString(
+  async configDeviceSettings(
       context: vscode.ExtensionContext, channel: vscode.OutputChannel,
       telemetryContext: TelemetryContext) {
     if (!vscode.workspace.rootPath) {
@@ -66,7 +66,7 @@ export class DeviceOperator {
       throw new Error(
           'Unable to set device connection string, please open an IoT Workbench project and retry.');
     }
-    await project.setDeviceConnectionString();
+    await project.configDeviceSettings();
   }
 
   async downloadPackage(
