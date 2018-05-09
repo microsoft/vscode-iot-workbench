b42cedb34c4eaf32168580a5c510168d359243db
Renhe Li <renhel@microsoft.com>
2018-05-09 17:06:30 +0800

    Add test case for ConfigHandler

---
+++ test/config.test.ts
@@ -0,0 +1,24 @@
+import * as assert from 'assert';
+import * as Path from 'path';
+import * as vscode from 'vscode';
+
+import {ConfigHandler} from '../src/configHandler';
+import {ConfigKey} from '../src/constants';
+
+
+suite('IoT Workbench: Config', () => {
+  test('should set and get config value correctly', function(done) {
+    this.timeout(60 * 1000);
+    try {
+      assert.equal(ConfigHandler.get<string>(ConfigKey.boardId), 'devkit');
+      assert.equal(ConfigHandler.get<string>(ConfigKey.devicePath), 'Device');
+      ConfigHandler.update(ConfigKey.boardId, 'IoTButton');
+      assert.equal(ConfigHandler.get<string>(ConfigKey.boardId), 'devkit');
+      ConfigHandler.update(ConfigKey.boardId, 'devkit');
+      done();
+
+    } catch (error) {
+      done(new Error(error));
+    }
+  });
+});
