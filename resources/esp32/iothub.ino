3d0dbf7a01a99a734bf41681dfd7ff1941997285
Renhe Li <renhel@microsoft.com>
2018-06-07 10:58:16 +0800

    update the  default program for esp32

--- resources/esp32/iothub.ino
+++ resources/esp32/iothub.ino
@@ -1,10 +1,9 @@
+/**
+ * A simple Azure IoT example for sending telemetry.
+ */
+
 #include <WiFi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "iothub_client.h"
-#include "iothub_message.h"
-#include "azure_c_shared_utility/platform.h"
-#include "iothubtransportmqtt.h"
+#include "Esp32MQTTClient.h"
 
 // Please input the SSID and password of WiFi
 const char* ssid     = "";
@@ -15,150 +14,47 @@ const char* password = "";
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
 static const char* connectionString = "";
 
-static char msgText[1024];
-static int callbackCounter;
-IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle;
-int receiveContext = 0;
-static char propText[1024];
-static size_t tackingIdIndex = 0;
-static bool confirmReceived = false;
-
-typedef struct EVENT_INSTANCE_TAG
-{
-    IOTHUB_MESSAGE_HANDLE messageHandle;
-    int messageTrackingId; // For tracking the messages within the user callback.
-} EVENT_INSTANCE;
-static EVENT_INSTANCE currentMessage;
-
-
-static unsigned char* bytearray_to_str(const unsigned char *buffer, size_t len)
-{
-    unsigned char* ret = (unsigned char*)malloc(len+1);
-    memcpy(ret, buffer, len);
-    ret[len] = '\0';
-    return ret;
-}
-
-static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* userContextCallback)
-{
-    EVENT_INSTANCE* eventInstance = (EVENT_INSTANCE*)userContextCallback;
-    size_t id = eventInstance->messageTrackingId;
-    
-    (void)printf("Confirmation[%d] received for message tracking id = %d with result = %s\r\n", callbackCounter, (int)id, ENUM_TO_STRING(IOTHUB_CLIENT_CONFIRMATION_RESULT, result));
-    /* Some device specific action code goes here... */
-    confirmReceived = true;
-    callbackCounter++;
-    IoTHubMessage_Destroy(eventInstance->messageHandle);
-}
-
-static IOTHUBMESSAGE_DISPOSITION_RESULT ReceiveMessageCallback(IOTHUB_MESSAGE_HANDLE message, void *userContextCallback)
-{
-    int *counter = (int *)userContextCallback;
-    const char *buffer;
-    size_t size;
-    MAP_HANDLE mapProperties;
-
-    // Message content
-    if (IoTHubMessage_GetByteArray(message, (const unsigned char **)&buffer, &size) != IOTHUB_MESSAGE_OK)
-    {
-        (void)printf("unable to retrieve the message data\r\n");
-    }
-    else
-    {
-        unsigned char* message_string = bytearray_to_str((const unsigned char *)buffer, size);
-        (void)printf("IoTHubMessage_GetByteArray received message: \"%s\" \n", message_string);
-        free(message_string);
-    }
-
-    /* Some device specific action code goes here... */
-    (*counter)++;
-    return IOTHUBMESSAGE_ACCEPTED;
-}
-
-void iot_hub_mqtt_example_init()
-{
-    callbackCounter = 0;
-    
-    srand((unsigned int)time(NULL));
-
-    if (platform_init() != 0)
-    {
-        (void)printf("Failed to initialize the platform.\r\n");
-        return;
-    }
-    
-    if ((iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString((char*)connectionString, MQTT_Protocol)) == NULL)
-    {
-        (void)printf("ERROR: iotHubClientHandle is NULL!\r\n");
-        return;
-    }
-
-    bool traceOn = true;
-    IoTHubClient_LL_SetOption(iotHubClientHandle, "logtrace", &traceOn);
-
-    /* Setting Message call back, so we can receive Commands. */
-    if (IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, ReceiveMessageCallback, &receiveContext) != IOTHUB_CLIENT_OK)
-    {
-        (void)printf("ERROR: IoTHubClient_LL_SetMessageCallback..........FAILED!\r\n");
-        return;
-    }
-}
-
-void iot_hub_mqtt_example_send_event(const unsigned char *text)
-{
-    currentMessage.messageHandle = IoTHubMessage_CreateFromByteArray(text, strlen((const char*)text));
-    currentMessage.messageTrackingId = tackingIdIndex;
-    tackingIdIndex++;
-    if (currentMessage.messageHandle == NULL) {
-        (void)printf("ERROR: iotHubMessageHandle is NULL!\r\n");
-        return;
-    }
-
-    MAP_HANDLE propMap = IoTHubMessage_Properties(currentMessage.messageHandle);
-    (void)sprintf_s(propText, sizeof(propText), "PropMsg_%d", currentMessage.messageTrackingId);
-    if (Map_AddOrUpdate(propMap, "PropName", propText) != MAP_OK)
-    {
-         (void)printf("ERROR: Map_AddOrUpdate Failed!\r\n");
-         return;
-    }
-
-    if (IoTHubClient_LL_SendEventAsync(iotHubClientHandle, currentMessage.messageHandle, SendConfirmationCallback, &currentMessage) != IOTHUB_CLIENT_OK)
-    {
-        (void)printf("ERROR: IoTHubClient_LL_SendEventAsync..........FAILED!\r\n");
-        return;
-    }
-    confirmReceived = false;
-    (void)printf("IoTHubClient_LL_SendEventAsync accepted messagefor transmission to IoT Hub.\r\n");
-}
+static bool hasIoTHub = false;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("Starting connecting WiFi.");
   delay(10);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  iot_hub_mqtt_example_init();
+
+  if (!Esp32MQTTClient_Init((const uint8_t*)connectionString))
+  {
+    hasIoTHub = false;
+    Serial.println("Initializing IoT hub failed.");
+    return;
+  }
+  hasIoTHub = true;
 }
 
 void loop() {
-    sprintf(msgText, "{\"topic\":\"%s\"}", "#iot");
-    iot_hub_mqtt_example_send_event((const unsigned char *)msgText);
-    while(true)
+  Serial.println("start sending events.");
+  if (hasIoTHub)
+  {
+    char buff[128];
+
+    // replace the following line with your data sent to Azure IoTHub
+    snprintf(buff, 128, "{\"topic\":\"iot\"}");
+    
+    if (Esp32MQTTClient_SendEvent(buff))
     {
-        IoTHubClient_LL_DoWork(iotHubClientHandle);
-        
-        if (confirmReceived)
-        {
-            // IoT Hub got this event
-            delay(5000);
-            return;
-        }
-        delay(100);
+      Serial.println("Sending data succeed");
     }
+    else
+    {
+      Serial.println("Failure...");
+    }
+    delay(5000);
+  }
 }
