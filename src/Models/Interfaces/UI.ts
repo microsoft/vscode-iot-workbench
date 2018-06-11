8ad61e2e3732e6118f5d7bf0067bcdd1a44874c1
dooriya <dol@microsoft.com>
2018-06-11 13:50:12 +0800

    fix issue #292: popup message is incorrect for device connection string configuration

--- src/Models/Interfaces/UI.ts
+++ src/Models/Interfaces/UI.ts
@@ -3,4 +3,6 @@
 
 import {QuickPickItem} from 'vscode';
 
-export interface PickWithData<T> extends QuickPickItem { data: T; }
+export interface PickWithData<T> extends QuickPickItem {
+  data: T;
+}
