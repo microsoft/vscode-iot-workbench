6ad8cc4a965b1fd3a15b6fae2c415208523f3969
Renhe Li <Renhe.Li@microsoft.com>
2019-03-22 01:51:03 +0000

    Merged PR 447728: Use aka link and modify the azure pipeline
    Use aka link and modify the azure pipeline

--- updateCodeGenLink.js
+++ updateCodeGenLink.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
-const codeGenUrl = process.env['prodCodeGenUrl'];
+const codeGenUrl = process.env['prodCodeGenUrl-v2'];
 if(!codeGenUrl){
-  throw new Error('Unable to find the variable of prodCodeGenUrl');
+  throw new Error('Unable to find the variable of prodCodeGenUrl-v2');
 }
 
 const packageJson = JSON.parse(fs.readFileSync('package.json'));
 const ISPROD = /^v?[0-9]+\.[0-9]+\.[0-9]+$/.test(packageJson.version);
   
 if (ISPROD) {
-  packageJson.codeGenConfigUrl = 'https://azureboard2.blob.core.windows.net/pnp/iot-pnp-config-staging-v2.json';
+  packageJson.codeGenConfigUrl = codeGenUrl;
   fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n');
 }
