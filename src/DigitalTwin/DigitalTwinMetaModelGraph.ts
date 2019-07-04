7ec32fc0216df8855ee8c41013e3845759ad49b2
Zhe Li <zlhe@microsoft.com>
2019-07-04 16:47:17 +0800

    fix implements schema

--- src/DigitalTwin/DigitalTwinMetaModelGraph.ts
+++ src/DigitalTwin/DigitalTwinMetaModelGraph.ts
@@ -438,11 +438,15 @@ export class DigitalTwinMetaModelParser {
   }
 
   getStringValuePattern(key: string) {
+    // urn:[namespace]:[name]:[version]
+    const urnPattern = /^urn:([a-zA-Z0-9_]+:)+[a-zA-Z0-9_]+:\d+$/;
     switch (key) {
       case '@id':
-        return /^urn:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+:\d+$/;
+        return urnPattern;
       case 'name':
         return /^[a-zA-Z0-9_]+$/;
+      case 'schema':
+        return urnPattern;
       // case 'implements':
       //   return /^(http|https):\/\/.+\.interface\/.json$/;
       default:
@@ -453,6 +457,8 @@ export class DigitalTwinMetaModelParser {
   getStringValueLengthRange(key: string) {
     switch (key) {
       case '@id':
+        return [0, 256];
+      case 'schema':
         return [0, 256];
       default:
         return null;
@@ -500,6 +506,8 @@ export class DigitalTwinMetaModelParser {
         return ['@type'];
       case 'CapabilityModel':
         return ['@id', '@type', '@context', 'implements'];
+      case 'InterfaceInstance':
+        return ['name', 'schema'];
       default:
         return [];
     }
