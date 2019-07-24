e8649568df0fe3e64dc934c1cbf5ce5e43593409
Renhe Li <Renhe.Li@microsoft.com>
2019-07-24 10:46:52 +0000

    Merged PR 489085: fix a ui issue to also filter urnId

--- DigitalTwinRepositoryViews/main.js
+++ DigitalTwinRepositoryViews/main.js
@@ -1,7 +1,10 @@
 var repository = new Vue({
-  el: '#main',
+  el: "#main",
   data: {
-    companyName: _location.search === '?public' ? 'Public repository' : 'Company repository',
+    companyName:
+      _location.search === "?public"
+        ? "Public repository"
+        : "Company repository",
     selectedInterfaces: {
       value: []
     },
@@ -15,22 +18,22 @@ var repository = new Vue({
       value: []
     },
     type: {
-      value: 'CapabilityModel'
+      value: "CapabilityModel"
     },
     interfaceNextToken: {
-      value: ''
+      value: ""
     },
     capabilityNextToken: {
-      value: ''
+      value: ""
     },
     showSearchBar: false,
     showStatusSelector: false,
     showTagSelector: false,
-    filterStatus: 'All',
+    filterStatus: "All",
     filterTags: [],
-    filterKeywords: '',
-    searchKeywords: '',
-    filterTagsOrAnd: 'and',
+    filterKeywords: "",
+    searchKeywords: "",
+    filterTagsOrAnd: "and",
     loadingDigitalTwinInterfaces: {
       value: true
     },
@@ -39,23 +42,23 @@ var repository = new Vue({
     },
     allTags: {
       value: [
-        'tag1',
-        'tag2',
-        'tag3',
-        'tag11',
-        'tag12',
-        'tag13',
-        'tag21',
-        'tag22',
-        'tag23',
-        'tag31',
-        'tag32',
-        'tag33'
+        "tag1",
+        "tag2",
+        "tag3",
+        "tag11",
+        "tag12",
+        "tag13",
+        "tag21",
+        "tag22",
+        "tag23",
+        "tag31",
+        "tag32",
+        "tag33"
       ]
     },
-    filterTagsKeywords: '',
+    filterTagsKeywords: "",
     nextPageLoadingCounter: null,
-    publicRepository: _location.search === '?public'
+    publicRepository: _location.search === "?public"
   },
   methods: {
     command,
@@ -65,8 +68,8 @@ var repository = new Vue({
       if (!filterKeywords) {
         return value;
       }
-      const filterReg = new RegExp(`(${filterKeywords})`, 'ig');
-      return value.replace(filterReg, '<em>$1</em>');
+      const filterReg = new RegExp(`(${filterKeywords})`, "ig");
+      return value.replace(filterReg, "<em>$1</em>");
     },
     createDigitalTwinFile,
     deleteDigitalTwinFiles,
@@ -89,13 +92,13 @@ var repository = new Vue({
     clearKeywords
   },
   created: function() {
-    getNextPageDigitalTwinFiles.call(this, 'Interface');
-    getNextPageDigitalTwinFiles.call(this, 'CapabilityModel');
+    getNextPageDigitalTwinFiles.call(this, "Interface");
+    getNextPageDigitalTwinFiles.call(this, "CapabilityModel");
   }
 });
 
 function encodeHTML(value) {
-  let div = document.createElement('div');
+  let div = document.createElement("div");
   div.innerText = value;
   const html = div.innerHTML;
   div = undefined;
@@ -103,50 +106,77 @@ function encodeHTML(value) {
 }
 
 function deleteDigitalTwinFiles() {
-  const fileIds = this.type.value === 'Interface' ? this.selectedInterfaces.value : this.selectedCapabilityModels.value;
-  command('iotworkbench.deleteMetamodelFiles', fileIds, this.type.value, refreshDigitalTwinFileList.bind(this));
+  const fileIds =
+    this.type.value === "Interface"
+      ? this.selectedInterfaces.value
+      : this.selectedCapabilityModels.value;
+  command(
+    "iotworkbench.deleteMetamodelFiles",
+    fileIds,
+    this.type.value,
+    refreshDigitalTwinFileList.bind(this)
+  );
 }
 
 function editDigitalTwinFiles() {
-  const fileIds = this.type.value === 'Interface' ? this.selectedInterfaces.value : this.selectedCapabilityModels.value;
-  command('iotworkbench.editMetamodelFiles', fileIds, this.type.value, this.publicRepository, refreshDigitalTwinFileList.bind(this));
+  const fileIds =
+    this.type.value === "Interface"
+      ? this.selectedInterfaces.value
+      : this.selectedCapabilityModels.value;
+  command(
+    "iotworkbench.editMetamodelFiles",
+    fileIds,
+    this.type.value,
+    this.publicRepository,
+    refreshDigitalTwinFileList.bind(this)
+  );
 }
 
 function createDigitalTwinFile() {
-  const commandName = this.type.value === 'Interface' ? 'iotworkbench.iotPnPCreateInterface' : 'iotworkbench.iotPnPCreateCapabilityModel';
+  const commandName =
+    this.type.value === "Interface"
+      ? "iotworkbench.iotPnPCreateInterface"
+      : "iotworkbench.iotPnPCreateCapabilityModel";
   command(commandName);
 }
 
 function getNextPageDigitalTwinFiles(fileType) {
-  fileType = typeof fileType === 'string' ? fileType : this.type.value;
+  fileType = typeof fileType === "string" ? fileType : this.type.value;
   let commandName, fileList, nextToken, loadingDigitalTwinFiles;
-  
-  if(fileType === 'Interface') {
-    commandName = 'iotworkbench.getInterfaces';
+
+  if (fileType === "Interface") {
+    commandName = "iotworkbench.getInterfaces";
     fileList = this.interfaceList;
     nextToken = this.interfaceNextToken;
     loadingDigitalTwinFiles = this.loadingDigitalTwinInterfaces;
-    tableId = 'interfaceListTable';
+    tableId = "interfaceListTable";
   } else {
-    commandName = 'iotworkbench.getCapabilityModels';
+    commandName = "iotworkbench.getCapabilityModels";
     fileList = this.capabilityList;
     nextToken = this.capabilityNextToken;
     loadingDigitalTwinFiles = this.loadingDigitalTwinCapabilityModels;
-    tableId = 'capabilityListTable';
+    tableId = "capabilityListTable";
   }
 
   loadingDigitalTwinFiles.value = true;
 
-  command(commandName, this.searchKeywords, this.publicRepository, 50, nextToken.value, res => {
-    Vue.set(fileList, 'value', fileList.value.concat(res.result.results));
-    Vue.set(nextToken, 'value', res.result.continuationToken);
-    Vue.set(loadingDigitalTwinFiles, 'value', false);
-  });
+  command(
+    commandName,
+    this.searchKeywords,
+    this.publicRepository,
+    50,
+    nextToken.value,
+    res => {
+      Vue.set(fileList, "value", fileList.value.concat(res.result.results));
+      Vue.set(nextToken, "value", res.result.continuationToken);
+      Vue.set(loadingDigitalTwinFiles, "value", false);
+    }
+  );
 }
 
 function refreshDigitalTwinFileList() {
   let nextToken, selectedList;
-  if(this.type.value === 'Interface') {
+  if (this.type.value === "Interface") {
     fileList = this.interfaceList;
     nextToken = this.interfaceNextToken;
     selectedList = this.selectedInterfaces;
@@ -158,10 +188,10 @@ function refreshDigitalTwinFileList() {
     loadingDigitalTwinFiles = this.loadingDigitalTwinCapabilityModels;
   }
 
-  Vue.set(fileList, 'value', []);
-  Vue.set(nextToken, 'value', '');
-  Vue.set(selectedList, 'value', []);
-  Vue.set(loadingDigitalTwinFiles, 'value', true);
+  Vue.set(fileList, "value", []);
+  Vue.set(nextToken, "value", "");
+  Vue.set(selectedList, "value", []);
+  Vue.set(loadingDigitalTwinFiles, "value", true);
   setTimeout(getNextPageDigitalTwinFiles.bind(this), 1000); // wait for server refresh
 }
 
@@ -183,9 +213,9 @@ function showHideTagSelector() {
 
 function clearFilter() {
   this.filterTags = [];
-  this.filterStatus = 'All';
+  this.filterStatus = "All";
   this.showStatusSelector = false;
-  this.filterKeywords = '';
+  this.filterKeywords = "";
   this.showTagSelector = false;
 }
 
@@ -234,11 +264,11 @@ function filterItems(list) {
   const filterTagsOrAnd = this.filterTagsOrAnd;
 
   return list.filter(item => {
-    if (filterStatus !== 'All') {
-      if (item.published && filterStatus!== 'Published') {
+    if (filterStatus !== "All") {
+      if (item.published && filterStatus !== "Published") {
         return false;
       }
-      if (!item.published && filterStatus!== 'Saved') {
+      if (!item.published && filterStatus !== "Saved") {
         return false;
       }
     }
@@ -252,7 +282,12 @@ function filterItems(list) {
     }
 
     const displayName = item.displayName;
-    if (filterKeywords && displayName.toLowerCase().indexOf(filterKeywords.toLowerCase()) === -1) {
+    const urnId = item.urnId;
+    if (
+      filterKeywords &&
+      displayName.toLowerCase().indexOf(filterKeywords.toLowerCase()) === -1 &&
+      urnId.toLowerCase().indexOf(filterKeywords.toLowerCase()) === -1
+    ) {
       return false;
     }
 
@@ -270,22 +305,22 @@ function isMatchTags(tags, selectedTags, orAnd) {
 
   for (tag of selectedTags) {
     const index = tags.indexOf(tag);
-    if (index === -1 && orAnd === 'and') {
+    if (index === -1 && orAnd === "and") {
       return false;
     }
-    if (index !== -1 && orAnd === 'or') {
+    if (index !== -1 && orAnd === "or") {
       return true;
     }
-    if (index !== -1 && orAnd === 'and') {
+    if (index !== -1 && orAnd === "and") {
       tags.splice(index, 1);
     }
   }
-  
-  if (orAnd === 'or') {
+
+  if (orAnd === "or") {
     return false;
   }
 
-  if (orAnd === 'and' && !tags.length) {
+  if (orAnd === "and" && !tags.length) {
     return true;
   }
 
@@ -296,9 +331,19 @@ function onScrollTable(event) {
   if (this.filterKeywords) {
     return;
   }
-  const nextToken = this.type.value === 'Interface' ? this.interfaceNextToken.value : this.capabilityNextToken.value;
-  const loadingDigitalTwinFiles = this.type.value === 'Interface' ? this.loadingDigitalTwinInterfaces : this.loadingDigitalTwinCapabilityModels;
-  if (!nextToken || this.nextPageLoadingCounter || loadingDigitalTwinFiles.value) {
+  const nextToken =
+    this.type.value === "Interface"
+      ? this.interfaceNextToken.value
+      : this.capabilityNextToken.value;
+  const loadingDigitalTwinFiles =
+    this.type.value === "Interface"
+      ? this.loadingDigitalTwinInterfaces
+      : this.loadingDigitalTwinCapabilityModels;
+  if (
+    !nextToken ||
+    this.nextPageLoadingCounter ||
+    loadingDigitalTwinFiles.value
+  ) {
     return;
   }
   this.nextPageLoadingCounter = setTimeout(() => {
@@ -313,22 +358,22 @@ function onScrollTable(event) {
 }
 
 function copy(event, content) {
-  const copyTextBox = document.createElement('input');
-  copyTextBox.className = 'copy-text-box';
+  const copyTextBox = document.createElement("input");
+  copyTextBox.className = "copy-text-box";
   copyTextBox.value = content;
   document.body.appendChild(copyTextBox);
   copyTextBox.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(copyTextBox);
-  event.target.className = 'copy_icon copied';
+  event.target.className = "copy_icon copied";
   setTimeout(() => {
-    event.target.className = 'copy_icon';
+    event.target.className = "copy_icon";
   }, 500);
 }
 
 function hasNoItemToPublish() {
   let selectedItemList, fullItemList;
-  if(this.type.value === 'Interface') {
+  if (this.type.value === "Interface") {
     fullItemList = this.interfaceList.value;
     selectedItemList = this.selectedInterfaces.value;
   } else {
@@ -347,12 +392,12 @@ function hasNoItemToPublish() {
 
 function searchOnServer() {
   this.searchKeywords = this.filterKeywords;
-  this.filterKeywords = '';
+  this.filterKeywords = "";
   this.refreshDigitalTwinFileList();
 }
 
 function clearKeywords() {
-  this.searchKeywords = '';
-  this.filterKeywords = '';
+  this.searchKeywords = "";
+  this.filterKeywords = "";
   this.refreshDigitalTwinFileList();
 }
