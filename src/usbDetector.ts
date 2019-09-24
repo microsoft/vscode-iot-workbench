64314f49f08bf74d6dc41e0a829d1acf8517cd75
JerryYangKai <kaiyan@microsoft.com>
2019-09-24 17:01:31 +0800

    travis ci problem

--- src/usbDetector.ts
+++ src/usbDetector.ts
@@ -71,7 +71,8 @@ export class UsbDetector {
                   new VSCExpress(this.context, 'views');
               UsbDetector._vscexpress.open(
                   exampleUrl,
-                  board.examplePageName + ' samples - Azure IoT Device Workbench',
+                  board.examplePageName +
+                      ' samples - Azure IoT Device Workbench',
                   vscode.ViewColumn.One, {
                     enableScripts: true,
                     enableCommandUris: true,
