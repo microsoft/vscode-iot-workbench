aeb7393022237f68e123d108a8ff86f523a80247
dilin-MS <dilin@microsoft.com>
2019-11-19 19:08:28 +0800

    Fix configure device connection string by removing ending concat inside serialport.js

--- src/Models/AZ3166Device.ts
+++ src/Models/AZ3166Device.ts
@@ -510,8 +510,7 @@ export class AZ3166Device extends ArduinoDeviceBase {
           const executeSetAzIoTHub = async () => {
             try {
               const data = `${command} "${configValue}"\r\n`;
-              // TODO: Remove this magic number
-              const maxDataLength = 160;
+              const maxDataLength = 256;
               await this.sendDataViaSerialPort(
                   port, data.slice(0, maxDataLength));
               if (data.length > maxDataLength) {
@@ -535,7 +534,7 @@ export class AZ3166Device extends ArduinoDeviceBase {
           // Configure serial port callbacks
           port.on('open', () => {
             port.write(
-                '\r\nhelp\r\n', AZ3166Device.serialport.endings.BothNLAndCR,
+                '\r\nhelp\r\n',
                 // tslint:disable-next-line: no-any
                 (error: any) => {
                   if (rejectIfError(error)) return;
@@ -588,7 +587,7 @@ export class AZ3166Device extends ArduinoDeviceBase {
                   .then(() => {
                     port.write(
                         '\r\nhelp\r\n',
-                        AZ3166Device.serialport.endings.BothNLAndCR,
+
                         // tslint:disable-next-line: no-any
                         (error: any) => {
                           rejectIfError(error);
@@ -664,7 +663,7 @@ export class AZ3166Device extends ArduinoDeviceBase {
         (resolve: (value: boolean) => void, reject: (value: Error) => void) => {
           try {
             port.write(
-                data, AZ3166Device.serialport.endings.BothNLAndCR,
+                data,
                 // tslint:disable-next-line: no-any
                 (err: any) => {
                   if (err) {
