81485eb2c2c56f87583fbf5c201dbb6c364db911
Di Lin <dilin@microsoft.com>
2020-01-09 16:57:24 +0800

    Merge pull request #903 from microsoft/move-to-azure-pipeline
    Move from travis ci to azure pipeline

--- scripts/updateConfig.js
+++ scripts/updateConfig.js
@@ -7,65 +7,43 @@ const fs = require('fs');
  * TRAVIS_TAG =~ /^v?[0-9]+\.[0-9]+\.[0-9]+$/:          Production release (eg. v0.10.18)
  * TRAVIS_TAG =~ /^v?[0-9]+\.[0-9]+\.[0-9]+-[rR][cC]/:  RC release (eg. v0.10.18-rc, v0.10.18-rc2, etc.)
  */
-if (process.env.TRAVIS_EVENT_TYPE === "cron" || process.env.TRAVIS_TAG) {
-  const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
 
-  // Nightly Build
-  if (process.env.TRAVIS_EVENT_TYPE === "cron") {
-    const nightlyBuildName = "test-owl-project-nightly";
-    const nightlyBuildDisplayName = "Test OWL Project (Nightly)";
-    const nightlyBuildPublisher = "IoTDevExBuild";
-    modifyPackageJsonForNonProduction(packageJson, nightlyBuildName, nightlyBuildDisplayName, nightlyBuildPublisher);
-  } else if (process.env.TRAVIS_TAG) {
-    const isProduction = /^v?[0-9]+\.[0-9]+\.[0-9]+$/.test(process.env.TRAVIS_TAG || '');
-    const isTestVersion = /^v?[0-9]+\.[0-9]+\.[0-9]+-[rR][cC]/.test(process.env.TRAVIS_TAG || '');
+// Nightly Build
+if (process.env.BUILD_REASON === "Schedule") {
+  const nightlyBuildName = "test-owl-project-nightly";
+  const nightlyBuildDisplayName = "Test OWL Project (Nightly)";
+  updateConfigForNonProduction(packageJson, nightlyBuildName, nightlyBuildDisplayName);
+} else if (process.env.IS_PROD) {
+  // Update resource link
+  const codeGenUrl = "https://aka.ms/iot-codegen-cli-for-workbench";
+  packageJson.codeGenConfigUrl = codeGenUrl;
 
-    if (isProduction) {
-      // Update resource link
-      const codeGenUrl = "https://aka.ms/iot-codegen-cli-for-workbench";
-      packageJson.codeGenConfigUrl = codeGenUrl;
-
-      // Update production AI Key
-      packageJson.aiKey = process.env['PROD_AIKEY'];
-    } else if (isTestVersion) {
-      const testName = "test-owl-project";
-      const testDisplayName = "Test OWL Project RC";
-      const testPublisher = "IoTDevExBuild";
-      modifyPackageJsonForNonProduction(packageJson, testName, testDisplayName, testPublisher);
-
-      // Modify extensionId in template files
-      const extensionIdPattern = /vsciot-vscode.vscode-iot-workbench/g;
-      const rcExtensionId = 'iotdevexbuild.test-owl-project';
-
-      const arm7DevcontainerJsonFile = "resources/templates/arm7/devcontainer.json";
-      const arm8DevcontainerJsonFile = "resources/templates/arm8/devcontainer.json";
-      const x86DevcontainerJsonFile = "resources/templates/x86/devcontainer.json";
-      const files = [arm7DevcontainerJsonFile, arm8DevcontainerJsonFile, x86DevcontainerJsonFile];
-      files.forEach(filePath => {
-        const originalJsonFile = fs.readFileSync(filePath).toString();
-        const replaceJson = originalJsonFile.replace(extensionIdPattern, rcExtensionId);
-        fs.writeFileSync(filePath, replaceJson);
-      });
-    }
-  }
-
-  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n');
+  // Update production AI Key
+  packageJson.aiKey = process.env.PROD_AIKEY;
+} else if (process.env.IS_TEST) {
+  const testName = "test-owl-project";
+  const testDisplayName = "Test OWL Project RC";
+  updateConfigForNonProduction(packageJson, testName, testDisplayName);
 }
+
+fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n');
 
 /**
  * Update package.json with test name, displayName, publisher, ai aky.
  * Trim version number. Delete extension icon.
+ * Update extension id in template files.
  * @param {*} packageJson package json oject
  * @param {*} testName test extension name
  * @param {*} testDisplayName test extension displate name
- * @param {*} testPublisher test publisher
  */
-function modifyPackageJsonForNonProduction(packageJson, testName, testDisplayName, testPublisher) {
+function updateConfigForNonProduction(packageJson, testName, testDisplayName) {
+  // Update package.json
   packageJson.name = testName;
   packageJson.displayName = testDisplayName;
-  packageJson.publisher = testPublisher;
+  packageJson.publisher = "IoTDevExBuild";
 
-  packageJson.aiKey = process.env['TEST_AIKEY'];
+  packageJson.aiKey = process.env.TEST_AIKEY;
 
   const indexOfDash = packageJson.version.indexOf('-');
   if (indexOfDash > 0) {
@@ -73,4 +51,18 @@ function modifyPackageJsonForNonProduction(packageJson, testName, testDisplayNam
   }
 
   delete packageJson.icon;
+
+  // Modify extensionId in template files
+  const extensionIdPattern = /vsciot-vscode.vscode-iot-workbench/g;
+  const testExtensionId = 'iotdevexbuild.' + testName;
+
+  const arm7DevcontainerJsonFile = "resources/templates/arm7/devcontainer.json";
+  const arm8DevcontainerJsonFile = "resources/templates/arm8/devcontainer.json";
+  const x86DevcontainerJsonFile = "resources/templates/x86/devcontainer.json";
+  const files = [arm7DevcontainerJsonFile, arm8DevcontainerJsonFile, x86DevcontainerJsonFile];
+  files.forEach(filePath => {
+    const originalJsonFile = fs.readFileSync(filePath).toString();
+    const replaceJson = originalJsonFile.replace(extensionIdPattern, testExtensionId.toLowerCase());
+    fs.writeFileSync(filePath, replaceJson);
+  });
 }
