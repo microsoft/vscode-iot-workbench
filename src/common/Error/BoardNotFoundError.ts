635cec72b5a80452a2e5356a9978e64a2017ce96
dilin-MS <dilin@microsoft.com>
2020-01-10 16:58:00 +0800

    change from gts check to eslint check in azure pipeline

--- src/common/Error/BoardNotFoundError.ts
+++
@@ -1,9 +0,0 @@
-import { InternalError } from "./InternalError";
-
-export class BoardNotFoundError extends InternalError {
-  constructor(board: string) {
-    const errorMessage = `${board} is not found in board list.`;
-    super(errorMessage);
-    this.name = "BoardNotFoundError";
-  }
-}
--- src/common/Error/TypeNotSupportedError.ts
+++
@@ -1,8 +0,0 @@
-import { InternalError } from "./InternalError";
-
-export class TypeNotSupportedError extends InternalError {
-  constructor(typeName: string, typeValue: string) {
-    super(`Unsupported ${typeName}: ${typeValue}`);
-    this.name = "TypeNotSupportedError";
-  }
-}
