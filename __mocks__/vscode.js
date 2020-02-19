09ce48677e1f359c33d7f7b9c25a938f7f12ffea
Eric Chen <erichen@microsoft.com>
2020-02-19 20:54:50 +0800

    test: add test case for device model manager

--- __mocks__/vscode.js
+++ __mocks__/vscode.js
@@ -1,16 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 const OutputChannel = {
   appendLine: jest.fn(),
   show: jest.fn(),
   dispose: jest.fn()
 };
 
+const ExtensionContext = {
+  asAbsolutePath: jest.fn(p => p)
+};
+
 const window = {
-  createOutputChannel: jest.fn(() => OutputChannel)
+  createOutputChannel: jest.fn(() => OutputChannel),
+  showInformationMessage: jest.fn(),
+  showWarningMessage: jest.fn(),
+  showErrorMessage: jest.fn()
 };
 
 const vscode = {
   window,
-  OutputChannel
+  OutputChannel,
+  ExtensionContext
 };
 
 module.exports = vscode;
