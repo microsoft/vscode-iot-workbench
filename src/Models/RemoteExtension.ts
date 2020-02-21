88f49da48deda5c6075c14ce8a62b79c01f6ed97
dilin-MS <dilin@microsoft.com>
2020-02-21 10:05:28 +0800

    Remove ExtensionId Enum

--- src/Models/RemoteExtension.ts
+++ src/Models/RemoteExtension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { ExtensionId, ExtensionName } from "./Interfaces/Api";
+import { ExtensionName } from "./Interfaces/Api";
 import { WorkbenchExtension } from "../WorkbenchExtension";
 import { RemoteEnvNotSupportedError } from "../common/Error/OperationFailedErrors/RemoteEnvNotSupportedError";
 import { OperationCanceledError } from "../common/Error/OperationCanceledError";
@@ -29,7 +29,7 @@ export class RemoteExtension {
     const res = await RemoteExtension.isAvailable();
     if (!res) {
       throw new OperationCanceledError(
-        `Remote extension is not available. Please install ${ExtensionId.Remote} first.`
+        `Remote extension is not available. Please install ${ExtensionName.Remote} first.`
       );
     }
   }
