c3604ff32bc124b9d7b00623f91cd9a50d56cc27
Eric Chen <erichen@microsoft.com>
2020-02-21 17:03:29 +0800

    test: add test case for device model manager (#923)
    - add example of mocking class from other module
    - add test case for "create interface/capability model"
    - add test case for UI interaction

--- __mocks__/vscode.js
+++ __mocks__/vscode.js
@@ -1,16 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 const OutputChannel = {
   appendLine: jest.fn(),
   show: jest.fn(),
   dispose: jest.fn()
 };
 
+const ExtensionContext = {
+  asAbsolutePath: jest.fn()
+};
+
+const Uri = {
+  fsPath: "defaultPath",
+  file: jest.fn()
+};
+
+const WorkspaceFolder = {
+  uri: Uri
+};
+
+const QuickPickOptions = {};
+
+const QuickPickItem = {};
+
+const OpenDialogOptions = {};
+
 const window = {
-  createOutputChannel: jest.fn(() => OutputChannel)
+  createOutputChannel: jest.fn(() => OutputChannel),
+  showQuickPick: jest.fn(items => items[0]),
+  showOpenDialog: jest.fn(),
+  showTextDocument: jest.fn(),
+  showInformationMessage: jest.fn(),
+  showWarningMessage: jest.fn(),
+  showErrorMessage: jest.fn()
+};
+
+const commands = {
+  executeCommand: jest.fn()
+};
+
+const workspace = {
+  workspaceFolders: undefined
 };
 
 const vscode = {
+  OutputChannel,
+  ExtensionContext,
+  Uri,
+  WorkspaceFolder,
+  QuickPickOptions,
+  QuickPickItem,
+  OpenDialogOptions,
   window,
-  OutputChannel
+  commands,
+  workspace
 };
 
 module.exports = vscode;
