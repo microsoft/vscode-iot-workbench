f4b1d07bb2e3ec267ce6c46a809876d03612928f
root <root@cn-dilin-1210.fareast.corp.microsoft.com>
2020-02-27 11:46:09 +0800

    Add unit tests for getting workbench path; Tiny fix

--- test/IoTSettings.test.ts
+++ test/IoTSettings.test.ts
@@ -3,6 +3,7 @@
 
 import * as path from "path";
 import { ConfigKey, OSPlatform } from "../src/constants";
+import { ConfigHandler } from "../src/configHandler";
 
 const vscode = require("../__mocks__/vscode");
 const { getHomeDir, getPlatform } = require("../src/utils");
@@ -10,85 +11,91 @@ const { getHomeDir, getPlatform } = require("../src/utils");
 jest.mock("../src/utils");
 jest.mock("../src/configHandler");
 
-let IoTWorkbenchSettingsModule: typeof import("../src/IoTSettings");
-let configHandlerModule: typeof import("../src/configHandler");
-const testHomeDir = "test-home-dir";
-
-beforeEach(() => {
-  // Set singleton modules as isolated module and they will be reseted every time
-  jest.isolateModules(() => {
-    IoTWorkbenchSettingsModule = require("../src/IoTSettings") as typeof import("../src/IoTSettings");
-    configHandlerModule = require("../src/configHandler") as typeof import("../src/configHandler");
-  });
-});
-
 describe("iot workbench settings", () => {
+  let IoTWorkbenchSettingsModule: typeof import("../src/IoTSettings");
+  const testHomeDir = "test-home-dir";
+  const emptyString = "";
+  const testWorkbenchPath = "test-workbench-path";
   getHomeDir.mockResolvedValue(testHomeDir);
+  getPlatform.mockResolvedValue(OSPlatform.LINUX);
+  const expectedDefaultWorkbenchPathOnLinux = path.join(testHomeDir, "IoTWorkbenchProjects");
+
+  beforeEach(() => {
+    // Set singleton modules as isolated module and they will be reseted every time
+    jest.isolateModules(() => {
+      IoTWorkbenchSettingsModule = require("../src/IoTSettings");
+    });
+  });
 
   test("get default workbench path on linux", async () => {
-    getPlatform.mockResolvedValueOnce(OSPlatform.LINUX);
     const defaultWorkbenchPath = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getDefaultWorkbenchPath();
-    expect(defaultWorkbenchPath).toEqual(path.join(testHomeDir, "IoTWorkbenchProjects"));
+    expect(defaultWorkbenchPath).toBe(expectedDefaultWorkbenchPathOnLinux);
   });
 
   test("get default workbench path on Win", async () => {
     getPlatform.mockResolvedValueOnce(OSPlatform.WIN32);
     const defaultWorkbenchPath = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getDefaultWorkbenchPath();
-    expect(defaultWorkbenchPath).toEqual(path.join(testHomeDir, "Documents", "IoTWorkbenchProjects"));
+    expect(defaultWorkbenchPath).toBe(path.join(testHomeDir, "Documents", "IoTWorkbenchProjects"));
   });
 
   test("get default workbench path on Mac", async () => {
     getPlatform.mockResolvedValueOnce(OSPlatform.DARWIN);
     const defaultWorkbenchPath = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getDefaultWorkbenchPath();
-    expect(defaultWorkbenchPath).toEqual(path.join(testHomeDir, "Documents", "IoTWorkbenchProjects"));
+    expect(defaultWorkbenchPath).toBe(path.join(testHomeDir, "Documents", "IoTWorkbenchProjects"));
   });
 
   test("get instance will update workbench path config with path from the config if not empty", async () => {
-    const workbenchPathFromConfig = "workbench-path-from-config";
-    const defaultWorkbenchPath = "default-workbench-path";
-    configHandlerModule.ConfigHandler.get = jest.fn().mockReturnValueOnce(workbenchPathFromConfig);
-    IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getDefaultWorkbenchPath = jest
-      .fn()
-      .mockResolvedValueOnce(defaultWorkbenchPath);
-
-    await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
-    expect(configHandlerModule.ConfigHandler.update).toBeCalledWith(
-      ConfigKey.workbench,
-      workbenchPathFromConfig,
-      vscode.ConfigurationTarget.Global
-    );
-  });
-
-  test("get instance will update workbench path config with default workbench path if path form config is empty", async () => {
-    const emptyString = "";
-    const testWorkbenchPath = "test-workbench-path";
-    configHandlerModule.ConfigHandler.get = jest.fn().mockReturnValueOnce(emptyString);
-    IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getDefaultWorkbenchPath = jest
-      .fn()
-      .mockResolvedValueOnce(testWorkbenchPath);
+    ConfigHandler.get = jest.fn().mockReturnValueOnce(testWorkbenchPath);
 
     await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
 
-    expect(configHandlerModule.ConfigHandler.update).toBeCalledWith(
+    expect(ConfigHandler.update).toHaveBeenCalledWith(
       ConfigKey.workbench,
       testWorkbenchPath,
       vscode.ConfigurationTarget.Global
     );
   });
 
-  test("set workbench path", async () => {
-    const userWorkbenchPath = "user-workbench-path";
-    vscode.window.showQuickPick = jest
-      .fn()
-      .mockResolvedValueOnce({ label: userWorkbenchPath, description: "", data: userWorkbenchPath });
+  test("get instance will update workbench path config with default workbench path if path from config is empty", async () => {
+    ConfigHandler.get = jest.fn().mockReturnValueOnce(emptyString);
+
+    await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
+
+    expect(ConfigHandler.update).toHaveBeenCalledWith(
+      ConfigKey.workbench,
+      expectedDefaultWorkbenchPathOnLinux,
+      vscode.ConfigurationTarget.Global
+    );
+  });
+
+  test("set workbench path with user workbench path", async () => {
+    ConfigHandler.get = jest.fn().mockReturnValueOnce(testWorkbenchPath);
 
     const settings = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
     await settings.setWorkbenchPath();
 
-    expect(configHandlerModule.ConfigHandler.update).toBeCalledWith(
+    expect(ConfigHandler.update).toHaveBeenCalledWith(
       ConfigKey.workbench,
-      userWorkbenchPath,
+      testWorkbenchPath,
       vscode.ConfigurationTarget.Global
     );
+  });
+
+  test("get workbench path from workspace config", async () => {
+    ConfigHandler.get = jest.fn().mockReturnValueOnce(testWorkbenchPath);
+
+    const settings = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
+    const newWorkbenchPath = await settings.getWorkbenchPath();
+
+    expect(newWorkbenchPath).toBe(testWorkbenchPath);
+  });
+
+  test("get workbench path when workspace config is empty", async () => {
+    ConfigHandler.get = jest.fn().mockReturnValueOnce(emptyString);
+
+    const settings = await IoTWorkbenchSettingsModule.IoTWorkbenchSettings.getInstance();
+    const newWorkbenchPath = await settings.getWorkbenchPath();
+
+    expect(newWorkbenchPath).toBe(expectedDefaultWorkbenchPathOnLinux);
   });
 });
